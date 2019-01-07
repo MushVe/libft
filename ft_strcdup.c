bd847259e48fdd88705c7579002957c7ff18429d
MushVe <c.seguiercuesta@free.fr>
2019-01-07 17:09:59 +0100

    libft

---
+++ ft_strcdup.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcdup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cseguier <cseguier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/18 16:59:32 by cseguier          #+#    #+#             */
+/*   Updated: 2018/12/18 18:21:02 by cseguier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcdup(char const *src, char c)
+{
+	char	*s;
+
+	if	(!(s = (char*)ft_memalloc((sizeof(*s) * (ft_strlen(src) + 1)))))
+		return (0);
+	return (ft_strccpy(s, src, c));
+}
